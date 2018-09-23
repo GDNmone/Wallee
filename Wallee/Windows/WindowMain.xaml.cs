0ba99046bd291b6403e4b51ccd0d569f811ef26e
Denis Gankov <dgankov@ntpcentr.com>
2018-09-23 17:10:09 +0300

    Base

---
+++ Wallee/Windows/WindowMain.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Windows;
+using System.Windows.Controls;
+using Wallee.Pages;
+
+namespace Wallee.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowMain.xaml
+    /// </summary>
+    public partial class WindowMain : System.Windows.Window, IBaseWindow
+    {
+        private object _sourcePage = new PageMain();
+
+        #region Events
+
+        public event EventHandler<UIElement> ChangePage;
+
+        #endregion
+
+        #region Dependetcy property
+
+
+        #endregion
+
+
+        public WindowMain()
+        {
+            InitializeComponent();
+        }
+
+        public object SourcePage
+        {
+            get => _sourcePage;
+            set
+            {
+                _sourcePage = value;
+                ChangePage?.Invoke(this, (UIElement)value);
+            }
+        }
+    }
+}
