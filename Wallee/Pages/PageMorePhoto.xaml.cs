387b64f0494515df2b390c36b870247edf091fe4
sttrox <sttrox@mail.ru>
2019-05-26 13:48:42 +0300

    Added BlackOut and save select Image

--- Wallee/Pages/PageMorePhoto.xaml.cs
+++ Wallee/Pages/PageMorePhoto.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using MaterialDesignColors;
+using Unsplasharp.Models;
 using Wallee.Utils;
 
 namespace Wallee.Pages
@@ -15,13 +13,62 @@ namespace Wallee.Pages
     /// </summary>
     public partial class PageMorePhoto : UserControl, INotifyPropertyChanged
     {
+        #region Property SelectPhoto(Photo)
+
+        private Photo _selectPhoto;
+
+        public Photo SelectPhoto
+        {
+            get { return _selectPhoto; }
+            set
+            {
+                _selectPhoto = value;
+                OnPropertyChanged(nameof(SelectPhoto));
+            }
+        }
+
+        #endregion
+
+        public static RoutedUICommand CommandSelectImage { get; } = new RoutedUICommand();
+
+        /// <summary>
+        /// Перенестив выбор следующее изображение
+        /// </summary>
+        public static RoutedUICommand CommandNextImage { get; } = new RoutedUICommand();
+
+        /// <summary>
+        /// Перенестив выбор предыдущее изображение
+        /// </summary>
+        public static RoutedUICommand CommandBackImage { get; } = new RoutedUICommand();
+
         public AsyncCommand SearchCommand { get; }
 
         public PageMorePhoto()
         {
             //SearchCommand = new AsyncCommand(ButtonBase_OnClickUpdata);
+            CommandBindings.Add(new CommandBinding(CommandSelectImage, Executed_SelectImage));
+            CommandBindings.Add(new CommandBinding(CommandNextImage, Executed_NextImage));
+            CommandBindings.Add(new CommandBinding(CommandBackImage, Executed_BackImage));
+
+            CommandNextImage.InputGestures.Add(new KeyGesture(Key.Right));
+            CommandBackImage.InputGestures.Add(new KeyGesture(Key.Left));
 
             InitializeComponent();
+        }
+
+        private void Executed_NextImage(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Executed_BackImage(object sender, ExecutedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Executed_SelectImage(object sender, ExecutedRoutedEventArgs e)
+        {
+            SelectPhoto = (Photo) e.Parameter;
         }
 
         #region Property TextSerch(string)
@@ -74,11 +121,5 @@ namespace Wallee.Pages
             countPage++;
             await ServiceUnsplash.GetPhoto(countPage, TextSerch, this.Dispatcher);
         }
-
-        
-    }
-
-    public static class CommandPageMorePhoto
-    {
     }
 }
---
+++ Wallee/UserControls/UserControlViewImage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Unsplasharp.Models;
+
+namespace Wallee.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UserControlViewImage.xaml
+    /// </summary>
+    public partial class UserControlViewImage : UserControl
+    {
+        public UserControlViewImage()
+        {
+            CommandBindings.Add(new CommandBinding(DownloadCommand, Executed_CommadnDownload));
+            DataContextChanged += (sender, args) => { sender = sender; };
+            InitializeComponent();
+        }
+
+
+        static UserControlViewImage()
+        {
+            #region Override DependencyProperties
+
+            #endregion
+
+            #region Initialization DependencyProperties
+
+            PhotoShowProperty = DependencyProperty.Register("PhotoShow",
+                typeof(Photo), typeof(UserControlViewImage),
+                new PropertyMetadata(null, PhotoShow_DependencyChange));
+
+            #endregion
+        }
+
+
+        #region DependencyProperties
+
+        #region Fields DependencyProperties
+
+        public static readonly DependencyProperty PhotoShowProperty;
+
+        #endregion
+
+        #region Properties DependencyProperties
+
+        public Photo PhotoShow
+        {
+            get { return (Photo) GetValue(PhotoShowProperty); }
+            set { SetValue(PhotoShowProperty, value); }
+        }
+
+        #endregion
+
+        #region Methods DependencyProperties
+
+        private static void PhotoShow_DependencyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as UserControlViewImage;
+            var value = (Photo) e.NewValue;
+            //if (value != null)
+            //    control.PhotoShow = value;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Commands
+
+        #region Methods Commands
+
+        private void Executed_CommadnDownload(object sender, ExecutedRoutedEventArgs e)
+        {
+            var t = PhotoShow;
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.FileName = t.Id + ".jpg";
+            if ((bool) dialog.ShowDialog())
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFileAsync(new Uri(t.Urls.Full), dialog.FileName);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Fields Commands
+
+        public static RoutedCommand DownloadCommand { get; } = new RoutedCommand();
+
+        #endregion
+
+        #endregion
+    }
+}
